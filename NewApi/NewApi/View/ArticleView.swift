2b0271f94a61ef8661d978a3e1fc5e438e223ecb
Macostik <y.granchenko@gmail.com>
2021-04-12 13:53:18 +0300

    create homeView, articleView

---
+++ NewApi/NewApi/View/ArticleView.swift
@@ -0,0 +1,20 @@
+//
+//  ArticleView.swift
+//  NewApi
+//
+//  Created by Maxim Granchenko on 12.04.2021.
+//
+
+import SwiftUI
+
+struct ArticleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArticleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArticleView()
+    }
+}
