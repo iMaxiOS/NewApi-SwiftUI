d63591a3e57679b68c3ae49ee04490a9e337b0be
Macostik <y.granchenko@gmail.com>
2021-04-12 11:35:37 +0300

    create project

---
+++ NewApi/NewApi/NewApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewApiApp.swift
+//  NewApi
+//
+//  Created by Maxim Granchenko on 12.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct NewApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
