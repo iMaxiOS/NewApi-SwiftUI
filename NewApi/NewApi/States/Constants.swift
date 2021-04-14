28257ce7ac4f15dd0726eb86d5eba567edb9336d
Macostik <y.granchenko@gmail.com>
2021-04-14 17:35:32 +0300

    implement SettingsView

---
+++ NewApi/NewApi/States/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  NewApi
+//
+//  Created by Maxim Granchenko on 14.04.2021.
+//
+
+import Foundation
+
+enum Constants {
+    static let twitter = "https://www.twitter.com/iMaxiOS"
+    static let email = "urk.maksim1@mail.com"
+    static let phone = "tel:0000000000007"
+}
--- NewApi/NewApi/View/SettingsView.swift
+++ NewApi/NewApi/View/SettingsView.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @State private var isToggle = false
+    @State private var isUserCustomSettings = false
+    
+    private let header = "Display"
+    private let footerString = "System settings will override Dark mode and use the current device theme"
+    
     var body: some View {
-        Text("Settings")
+        NavigationView {
+            Form {
+                Section(header: Text(header), footer: Text(footerString)) {
+                    Toggle(isOn: $isToggle, label: {
+                        Text("Dark mode")
+                    })
+                    
+                    Toggle(isOn: $isUserCustomSettings, label: {
+                        Text("Use system settings")
+                    })
+                }
+                
+                Section {
+                    Link(destination: URL(string: Constants.twitter)!, label: {
+                        Label("Follow me on Twitter @iMaxiOS", systemImage: "link")
+                    })
+                    Link("Contact me via email", destination: URL(string: Constants.email)!)
+                    Link("Call me", destination: URL(string: Constants.phone)!)
+                }
+                .foregroundColor(.black)
+                .font(.headline)
+            }
+            .navigationTitle("Settings")
+        }
     }
 }
 
